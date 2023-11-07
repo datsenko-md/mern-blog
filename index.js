7ca16e1a0e8caf2905478834672814f935113cb9
Max Datsenko <datsenko.md@gmail.com>
2023-11-07 07:55:12 +0200

    add login route

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {
