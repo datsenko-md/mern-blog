9a5dfe0577e8338e38ae6dd267d67671d2dde31e
Max Datsenko <datsenko.md@gmail.com>
2023-11-07 09:13:31 +0200

    add saving user

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
 
 import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 mongoose
   .connect('mongodb+srv://datsenkomd:gzR69eNgGTWOPlQZ@cluster0.gang369.mongodb.net/?retryWrites=true&w=majority')
@@ -14,14 +16,27 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/register', registerValidation, (req, res) => {
+app.post('/register', registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json(errors.array());
   }
 
+  const { password } = req.body;
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.compare.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
   res.json({
-    success: true,
+    user,
   });
 });
 
