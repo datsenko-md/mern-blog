89393032731032752c505d3aba94b9275a5bbb21
Max Datsenko <datsenko.md@gmail.com>
2023-11-07 16:49:13 +0200

    add posts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-import { registerValidation } from './validations/auth.js';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 mongoose
   .connect('mongodb+srv://datsenkomd:gzR69eNgGTWOPlQZ@cluster0.gang369.mongodb.net/mern_blog?retryWrites=true&w=majority')
@@ -17,6 +18,12 @@ app.use(express.json());
 app.post('/register', registerValidation, UserController.register);
 app.post('/login', loginValidation, UserController.login);
 app.get('/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
