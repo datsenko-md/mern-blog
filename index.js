33e9331cc4b41f0775182dfbcf86a343ad07670d
Max Datsenko <datsenko.md@gmail.com>
2023-11-07 12:58:00 +0200

    refactor user register

--- index.js
+++ index.js
@@ -17,31 +17,48 @@ const app = express();
 app.use(express.json());
 
 app.post('/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors.array());
+    }
+
+    const { password } = req.body;
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось зарегистрироваться',
+    });
   }
-
-  const { password } = req.body;
-  const salt = await bcrypt.genSalt();
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json({
-    user,
-  });
 });
-
-app.post('/login', (req, res) => {
-  console.log(req.body);
 
   const token = jwt.sign(
     {
